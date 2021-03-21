9444e04505f7570bdca147044d7be73aa9cf3074
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-03-20 23:15:54 -0400

    Initial commit

---
+++ rps/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dale Stmarthe</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+</div>
+</body>
+
